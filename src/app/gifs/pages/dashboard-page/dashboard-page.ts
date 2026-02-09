64d856130678aeb85f2e9438671cc41829abfb18
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2026-02-09 14:30:52 +0100

    configuracion de rutas y separacion de componentes del dashboard

--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard-page',
-  imports: [],
-  templateUrl: './dashboard-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class DashboardPage { }
