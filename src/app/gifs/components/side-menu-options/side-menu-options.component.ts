280a7ef73716ed5ae222342d61919621199e62ce
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2026-02-10 21:20:23 +0100

    creamos los enlaces a traves de objetos de manera mas dinamica

--- src/app/gifs/components/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 interface MenuOption{
   label: string;
   sublabel: string;
-  router: string;
+  route: string;
   icon: string;
 }
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent { 
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-house',
+      label: 'Trending',
+      sublabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+  },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ]
+}
