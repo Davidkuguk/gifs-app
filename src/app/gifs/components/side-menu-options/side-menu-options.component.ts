cf8d84fe30483c71f84efcc32330576f1bc723a3
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2026-02-10 19:22:52 +0100

    creamos la interface

--- src/app/gifs/components/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface MenuOption{
+  label: string;
+  sublabel: string;
+  router: string;
+  icon: string;
+}
+
 @Component({
   selector: 'gifs-side-menu-options',
   imports: [],
