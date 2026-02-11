4f6fbd0f22c5a752b01c0e6130b6faf7b16d029e
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2026-02-11 19:17:34 +0100

    refactorizacion del codigo y presentacion de imagenes en el trending page

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu.component.ts
@@ -1,7 +1,7 @@
 import {Component } from '@angular/core';
-import { SideMenuHeaderComponent } from '../side-menu-header/side-menu-header.component';
-import { SideMenuOptionsComponent } from '../side-menu-options/side-menu-options.component';
-import { environment } from '../../../../environments/environment';
+import { SideMenuHeaderComponent } from './side-menu-header/side-menu-header.component';
+import { SideMenuOptionsComponent } from './side-menu-options/side-menu-options.component';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'gifs-side-menu',
--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,13 +1,12 @@
 import {Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenuHeaderComponent } from '../../components/side-menu-header/side-menu-header.component';
-import { SideMenuOptionsComponent } from '../../components/side-menu-options/side-menu-options.component';
 import { GifsSideMenu } from '../../components/gifs-side-menu/gifs-side-menu.component';
-
 @Component({
   selector: 'app-dashboard-page',
   imports: [RouterOutlet,  GifsSideMenu],
   standalone:true,
   templateUrl: './dashboard-page.component.html',
 })
-export default class DashboardPageComponent { }
+export default class DashboardPageComponent {
+
+ }
